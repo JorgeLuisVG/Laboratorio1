4a3c885677c8cbf8cf3c2a54a8eafdcbada6ca49
JorgeLuisVG <158310235+JorgeLuisVG@users.noreply.github.com>
2024-07-29 09:04:44 -0600

    Se creo el metodo para ingresar productos

--- Program.cs
+++ Program.cs
@@ -9,8 +9,29 @@ static void Menu()
     Console.WriteLine("3 = Aplicar descuento");
 }
 
-static void IngresarProductos()
+static void IngresarProductos(string nombreProducto, double precioProducto, int contador = 0)
 {
+    int nuevo = 0;
+    do
+    {
+        contador++;
+        Console.WriteLine("Ingrese el nombre del producto");
+        nombreProducto = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el precio del producto");
+        precioProducto = Convert.ToDouble(precioProducto);
+
+        for (int i = 0; i < contador; i++)
+        {
+            
+
+        }
+
+        Console.WriteLine("Desea ingresar otro producto?");
+        Console.WriteLine("1 = SI");
+        Console.WriteLine("2 = NO");
+        nuevo = Convert.ToInt32(Console.ReadLine());
+    } while (nuevo == 1);
 }
 
 static void Pagar()
