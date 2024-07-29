7a0ac732934f3e031e8741281c4628d1e9d22921
JorgeLuisVG <158310235+JorgeLuisVG@users.noreply.github.com>
2024-07-29 08:43:13 -0600

    Se creo la opcion del menú

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Tienda Panfila");
+
+static void Menu();
+{
+    Console.WriteLine("1 = Ingresar producto");
+    Console.WriteLine("2 = Ingresar producto");
+    Console.WriteLine("3 = Ingresar producto");
+}
